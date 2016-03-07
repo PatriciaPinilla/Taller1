a9e6ef6969bdea06ee9704b5b4c4c9bb411d0bbe
PatriciaPinilla <claupinilla@uan.edu.co>
2016-03-06 19:42:07 -0500

    Paquete Completo

--- app/src/main/java/com/example/winchester/taller1/MainActivity.java
+++ app/src/main/java/com/example/winchester/taller1/MainActivity.java
@@ -7,20 +7,22 @@ import android.content.DialogInterface;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ImageView;;
+import android.widget.ImageView;
+import android.content.Intent;
+import android.view.Menu;
 
-    public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     public enum Option {
-      piedra, papel, tijeras
+        piedra, papel, tijeras
     }
 
     public enum Result {
-     GANASTE, PERDISTE, EMPATAMOS
+        GANASTE, PERDISTE, EMPATAMOS
     }
 
-private Option userSelection;
-private Result gameResult;
+    private Option userSelection;
+    private Result gameResult;
 
 
     @Override
@@ -28,41 +30,46 @@ private Result gameResult;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button piedra;
-        piedra = (Button) findViewById(R.id.ImgPiedra);
-        Button papel;
-        papel = (Button) findViewById(R.id.ImgPapel);
-        Button tijera;
-        tijera = (Button) findViewById(R.id.ImgTijera);
+        Button piedra  = (Button) findViewById(R.id.piedra);
+        Button papel = (Button) findViewById(R.id.papel);
+        Button tijera = (Button) findViewById(R.id.tijera);
+        Button inicio = (Button) findViewById(R.id.Inicio);
 
-        // Set click listening event for all buttons.
-        piedra.setOnClickListener((OnClickListener) this);
-        papel.setOnClickListener((OnClickListener)this);
-        tijera.setOnClickListener((OnClickListener)this);
+        piedra.setOnClickListener(this);
+        papel.setOnClickListener(this);
+        tijera.setOnClickListener(this);
+        inicio.setOnClickListener(this);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
     public void onClick(View v) {
 
-        ImageView imageView = (ImageView) findViewById(R.id.ImgPiedra);
+        ImageView imageView = (ImageView) findViewById(R.id.imageViewAnswerUser);
         boolean play = true;
 
         switch (v.getId()) {
-            case R.id.ImgPiedra:
+            case R.id.piedra:
                 userSelection = Option.piedra;
-              // imageView.setImageResource(R.drawable.piedra);
+                imageView.setImageResource(R.drawable.piedra);
                 break;
-            case R.id.ImgPapel:
+            case R.id.papel:
                 userSelection = Option.papel;
-              //  imageView.setImageResource(R.drawable.papel);
+                imageView.setImageResource(R.drawable.papel);
                 break;
             case R.id.tijera:
                 userSelection = Option.tijeras;
-             //   imageView.setImageResource(R.drawable.tijera);
+                imageView.setImageResource(R.drawable.tijera);
                 break;
-          //  case R.id.imageButtonHome:
-           //     startActivity(new Intent(activity_main.this, ChooseActivity.class)); // To go home.
-            //    play = false;
-              //  break;
+            case R.id.Inicio:
+                startActivity(new Intent(MainActivity.this, MainActivity.class)); // ir al inicio.
+                play = false;
+                break;
         }
 
         if(play) {
@@ -77,17 +84,16 @@ private Result gameResult;
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                // Do nothing
             }
         });
 
-        // Sets the right message according to result.
+        // mostrar resultado.
         if(gameResult == Result.PERDISTE) {
-            builder.setMessage("You Loose!");
+            builder.setMessage("PERDISTE!");
         } else if(gameResult == Result.GANASTE) {
-            builder.setMessage("You Win!");
+            builder.setMessage("GANASTE!");
         } else if(gameResult == Result.EMPATAMOS) {
-            builder.setMessage("It's a draw!");
+            builder.setMessage("EMPATE!");
         }
 
         AlertDialog alert = builder.create();
@@ -95,29 +101,28 @@ private Result gameResult;
     }
 
     private void play() {
-        // Generates a random play.
+        // Opciones de la máquina.
         int rand =  ((int)(Math.random() * 10)) % 3;
         Option androidSelection = null;
-     //   ImageView imageView = (ImageView) findViewById(R.id.ImageViewAnswerAndroid);
+        ImageView imageView = (ImageView) findViewById(R.id.ImageViewAnswerAndroid);
 
-        // Sets the right image according to random selection.
         switch (rand) {
             case 0:
                 androidSelection = Option.piedra;
-                //imageView.setImageResource(R.drawable.ImgPiedra);
+                imageView.setImageResource(R.drawable.piedra);
                 break;
             case 1:
                 androidSelection = Option.papel;
-                //imageView.setImageResource(R.drawable.papel);
+                imageView.setImageResource(R.drawable.papel);
                 break;
             case 2:
                 androidSelection = Option.tijeras;
-               // imageView.setImageResource(R.drawable.tijera);
+                imageView.setImageResource(R.drawable.tijera);
                 break;
         }
-        // Determine game result according to user selection and Android selection.
+
         if(androidSelection == userSelection) {
-            gameResult = Result.EMPATAMOS;
+            gameResult = Result.PERDISTE;
         }
         else if(androidSelection == Option.piedra && userSelection == Option.tijeras) {
             gameResult = Result.PERDISTE;
